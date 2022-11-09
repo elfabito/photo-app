7440a8a88d315db11830c20801949c7b6eb27455
Fab <info@desane.com.uy>
2022-11-09 19:10:34 -0300

    heroku

--- config/application.rb
+++ config/application.rb
@@ -10,15 +10,7 @@ module PhotoApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    ActionMailer::Base.smtp_settings = {
-      :address => 'smtp.sendgrid.net',
-      :port => '587',
-      :authentication => :plain,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'heroku.com',
-      :enable_starttls_auto => true
-    }
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
